9a22a1c56f2e3f9bd18390d797043dfdd4265c87
Jeremymcg88 <jeremy.mcguirk@gmail.com>
2021-07-04 05:40:30 -0400

    added to script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,78 @@
-// Assignment code here
-
-
-// Get references to the #generate element
+// Assignment Code
+// document.querySelector will return the "#generate" located in index.html
 var generateBtn = document.querySelector("#generate");
 
+// Assigned variables which will later return chosenCharacters = ""
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var special = "!@,#$%&*{}[]/\\+=";
+var chosenCharacters = "";
+
 // Write password to the #password input
-function writePassword() {
+function enterpassword() {
+var passwordText = document.querySelector("#password");
+var passwordTextvalue = password;
+}
+
+//write password function 
+function writePassword(){
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
+
+//generate password function gets called in writePassword function, and it should return the final password
+function generatePassword(){
+  //do random generation here and return the final password in the end
+  var result = "";
+  //Created var length with isNaN returning the value of "False" if the choosen numbers inside array are outside of parameter.
+  //Will then send window.alert to input number selection amount
+  var length = prompt("How many characters in your random password? (between 8 and 128)");
+  if(isNaN(length)){
+    alert("You must input a number!");
+    return generatePassword()
+  }
+  if(length<8 || length> 128){
+    alert("Please choose numbers between 8 - 128!");
+    return generatePassword()
+  }
+  //ask the user if they want to include upper, lower, num, special with window.confirm
+  var hasUpper = confirm("Include upper case letters?");
+  var hasLower = confirm("Include lower case letters?");
+  var hasNumbers = confirm("Include numbers?");
+  var hasSpecial = confirm("Include special characters?");
+
+  //logical AND statement to determine what to include in password
+  if(!hasUpper&&!hasLower&&!hasNumbers&&!hasSpecial){
+    alert("You must at least choose 1 character type!");
+    return generatePassword()
+  }
+
+  if(hasUpper){
+    chosenCharacters += upper
+  }
+  if(hasLower){
+    chosenCharacters += lower
+  }
+
+  if(hasNumbers) {
+    chosenCharacters += numbers
+  }
+
+  if(hasSpecial) {
+    chosenCharacters += special
+  }
+
+  //for loop that will return the array and loop through all "chosenCharacters"
+  //returning the generated password 
+  // var i = 0; i < length; i++ meaning using the current value and then incrementing the value of "i" by 1.
+for (var i = 0; i < length; i++) {
+  result += chosenCharacters.charAt(Math.floor(Math.random() * chosenCharacters.length));
+}
+return result;
+
+}
